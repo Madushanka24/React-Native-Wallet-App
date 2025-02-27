64984782f2dd36be93e0397e3440d88236d4d1dc
Madushanka24 <madushanka20021224@gmail.com>
2025-02-27 13:14:12 +0530

    shared.tsx

--- components/colors.tsx
+++ components/colors.tsx
@@ -1,4 +1,10 @@
 export const colors = {
-    white: "#fff"
-    
-}
+    white: "#fff",
+    primary :"#ef835d" ,
+    secondary: "#2c365a",
+    tertiary: "#85c6d8",
+    gar: "#d1d5db",
+    graylight: "#F3F4F6",
+    graydark: "#4B5563",
+    accent: "#fbcd77",
+};
